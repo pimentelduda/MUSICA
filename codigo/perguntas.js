81272182fca7a8d20cb233dc8aeb88986f0dd9db
pimentelduda <148362188+pimentelduda@users.noreply.github.com>
2024-09-27 09:57:09 -0300

    Add files via upload

---
+++ codigo/perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    'MÚSICA ',
+    'Quais são os 7 elementos da música ',
+    'melodia,harmonia,ritimo de som, altura,intensidade,timbre  duração?'
+)
+
+
+criaCartao(
+    'MÚSICA',
+    'Aonde surgiu a música ?',
+    ' Mesopotâmia'
+)
+
+
+criaCartao(
+    'MÚSICA',
+    'Qual a coisa mais importate da música ?',
+    'A melodia '
+)
+
+
+criaCartao(
+    'MÚSICA',
+    'Qual e o maior musico do mundo ?',
+    'Bob Dylan'
+)
